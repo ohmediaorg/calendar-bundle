ae448f098b8b26c368ebfe490dfef3dbf186d3e9
justin-oh <justin@ohmedia.ca>
2026-07-10 10:33:10 -0600

    remove unneeded property

--- src/Data/CalendarTag.php
+++ src/Data/CalendarTag.php
@@ -8,7 +8,6 @@ class CalendarTag
         public readonly string $text,
         public readonly string $id,
         public readonly string $backgroundColor,
-        public readonly string $borderColor,
         public readonly string $textColor,
     ) {
     }
